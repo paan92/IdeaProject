0a55518f21f6a58cc1970368783a48d6ce055e2e
paan92 <parshihin@inbox.ru>
2017-01-27 19:28:49 +0300

    ...

--- GwtTutorial/src/com/mySampleApplication/client/MySampleApplication.java
+++ GwtTutorial/src/com/mySampleApplication/client/MySampleApplication.java
@@ -33,6 +33,7 @@ public class MySampleApplication implements EntryPoint {
         stocksFlexTable.setText(0, 3, "Remove");
 
         // Add styles to elements in the stock list table.
+        stocksFlexTable.setCellPadding(6);
         stocksFlexTable.getRowFormatter().addStyleName(0, "watchListHeader");
         stocksFlexTable.addStyleName("watchList");
         stocksFlexTable.getCellFormatter().addStyleName(0, 1, "watchListNumericColumn");
@@ -140,8 +141,19 @@ public class MySampleApplication implements EntryPoint {
 
         // Populate the Price and Change fields with new data.
         stocksFlexTable.setText(row, 1, priceText);
-        stocksFlexTable.setText(row, 2, changeText + " (" + changePercentText
-                + "%)");
+        Label changeWidget = (Label)stocksFlexTable.getWidget(row, 2);
+        changeWidget.setText(changeText + " (" + changePercentText + "%)");
+
+        // Change the color of text in the Change field based on its value.
+        String changeStyleName = "noChange";
+        if (price.getChangePercent() < -0.1f) {
+            changeStyleName = "negativeChange";
+        }
+        else if (price.getChangePercent() > 0.1f) {
+            changeStyleName = "positiveChange";
+        }
+
+        changeWidget.setStyleName(changeStyleName);
     }
 
     private void addStock() {
@@ -165,6 +177,7 @@ public class MySampleApplication implements EntryPoint {
         int row = stocksFlexTable.getRowCount();
         stocks.add(symbol);
         stocksFlexTable.setText(row, 0, symbol);
+        stocksFlexTable.setWidget(row, 2, new Label()); //виджет внутри ячейки
         stocksFlexTable.getCellFormatter().addStyleName(row, 1, "watchListNumericColumn");
         stocksFlexTable.getCellFormatter().addStyleName(row, 2, "watchListNumericColumn");
         stocksFlexTable.getCellFormatter().addStyleName(row, 3, "watchListRemoveColumn");
@@ -172,6 +185,7 @@ public class MySampleApplication implements EntryPoint {
 
         // Add a button to remove this stock from the table.
         Button removeStockButton = new Button("x");
+        removeStockButton.addStyleDependentName("remove");              //вторичный стиль
         removeStockButton.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
                 int removedIndex = stocks.indexOf(symbol);
